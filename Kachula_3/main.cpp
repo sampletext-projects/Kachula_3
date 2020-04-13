a3160338b15a9e74f0dd8ba3c5345a93516edb33
sampletext32 <sampletext32@bk.ru>
2020-04-13 08:49:03 +0300

    Add project

---
+++ Kachula_3/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	int n1, n2;
+	cout << "N1: ";
+	cin >> n1;
+	cout << "N2: ";
+	cin >> n2;
+
+	for (int i = n1; i <= n2; ++i)
+	{
+		int value = i; //копируем переменную, чтобы не изменять внутри проверки
+		int reversed = 0; //разворачиваем число используя 10 систему счисления
+		do
+		{
+			reversed = reversed * 10 + value % 10;
+			value /= 10;
+		}
+		while (value != 0);
+		
+		if (reversed == i)
+		{
+			cout << i << " Палиндром\n";
+		}
+	}
+
+	system("pause");
+}
