ba75a42daf482e73444f4270539a6250a9008f46
sampletext32 <sampletext32@bk.ru>
2020-05-07 17:35:58 +0300

    Add code repeat

--- Kachula_3/main.cpp
+++ Kachula_3/main.cpp
@@ -6,28 +6,33 @@ int main()
 {
 	setlocale(LC_ALL, "russian");
 
-	int n1, n2;
-	cout << "N1: ";
-	cin >> n1;
-	cout << "N2: ";
-	cin >> n2;
-
-	for (int i = n1; i <= n2; ++i)
+	int r;
+	do
 	{
-		int value = i; //копируем переменную, чтобы не изменять внутри проверки
-		int reversed = 0; //разворачиваем число используя 10 систему счисления
-		do
+		int n1, n2;
+		cout << "N1: ";
+		cin >> n1;
+		cout << "N2: ";
+		cin >> n2;
+
+		for (int i = n1; i <= n2; ++i)
 		{
-			reversed = reversed * 10 + value % 10;
-			value /= 10;
+			int value = i; //копируем переменную, чтобы не изменять внутри проверки
+			int reversed = 0; //разворачиваем число используя 10 систему счисления
+			do
+			{
+				reversed = reversed * 10 + value % 10;
+				value /= 10;
+			} while (value != 0);
+
+			if (reversed == i)
+			{
+				cout << i << " Палиндром\n";
+			}
 		}
-		while (value != 0);
-		
-		if (reversed == i)
-		{
-			cout << i << " Палиндром\n";
-		}
-	}
+		cout << "Повторить(1)?:";
+		cin >> r;
+	} while (r == 1);
 
 	system("pause");
 }
